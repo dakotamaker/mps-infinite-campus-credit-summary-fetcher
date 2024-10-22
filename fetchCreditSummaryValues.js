46bda8350102440c5fed68e79df8f484114e984a
Dakota Maker <dakotamaker@gmail.com>
2024-10-21 21:47:06 -0500

    Include student name in table
    Enhancing the readability and reliability of the table by adding in the student name first

--- fetchCreditSummaryValues.js
+++ fetchCreditSummaryValues.js
@@ -3,23 +3,30 @@ const getCredits = () => {
     let initialIframeDocument = initialIframe.contentDocument || initialIframe.contentWindow.document;
     
     let workspaceWrapperIframe = initialIframeDocument.querySelector('#frameWorkspaceWrapper')
-    let  workspaceWrapperDocument = workspaceWrapperIframe.contentDocument || workspaceWrapperIframe.contentWindow.document;
+    let workspaceWrapperDocument = workspaceWrapperIframe.contentDocument || workspaceWrapperIframe.contentWindow.document;
  
+    let workspaceHeaderIframe =  workspaceWrapperDocument.querySelector("#frameWorkspaceHeader")
+    let workspaceHeaderDocument = workspaceHeaderIframe.contentDocument || workspaceHeaderIframe.contentWindow.document;
+
+    let studentNameElement = workspaceHeaderDocument.querySelector('.topBar h1')
+    let studentName = studentNameElement.innerText.trim()
+
     let workspaceDetailIframe = workspaceWrapperDocument.querySelector('#frameWorkspaceDetail')
     let workspaceDetailDocument = workspaceDetailIframe.contentDocument || workspaceWrapperIframe.contentWindow.document;
  
      let result = {
-         "01 - English": 0,
-         "02 - Math": 0,
-         "03 - Science": 0,
-         "04 - Social Studies": 0,
-         "05 - Health": 0,
-         "06 - Phy Ed": 0,
-         "07 - Fine Arts": 0,
-         "08 - Electives": 0,
-         "09 - Service Learning": 0,
-         "10 - World Language": 0,
-         "11 - Career Readiness": 0
+        "00 - Student Name": studentName,
+        "01 - English": 0,
+        "02 - Math": 0,
+        "03 - Science": 0,
+        "04 - Social Studies": 0,
+        "05 - Health": 0,
+        "06 - Phy Ed": 0,
+        "07 - Fine Arts": 0,
+        "08 - Electives": 0,
+        "09 - Service Learning": 0,
+        "10 - World Language": 0,
+        "11 - Career Readiness": 0
      }
  
      const tableValueArrays = Array.from(workspaceDetailDocument .querySelectorAll('.detailFormColumn')).map(node => node.parentElement.textContent.replace(/ /g,'').split('\n'))
