727681a9b45b5bcda431d901731dd71baca0ea24
Dakota Maker <dakotamaker@gmail.com>
2024-10-22 19:02:29 -0500

    Add total credit value for extra information

--- fetchCreditSummaryValues.js
+++ fetchCreditSummaryValues.js
@@ -28,51 +28,58 @@ const getCredits = () => {
           "08 - Electives": 0,
           "09 - Service Learning": 0,
           "10 - World Language": 0,
-          "11 - Career Readiness": 0
+          "11 - Career Readiness": 0,
+          "12 - Total": 0
      }
 
-     const tableValueArrays = Array.from(workspaceDetailDocument .querySelectorAll('.detailFormColumn')).map(node => node.parentElement.textContent.replace(whitespaceRegex,'').split('\n'))
+     const tableValueArrays = Array.from(workspaceDetailDocument.querySelectorAll('.detailFormColumn')).map(node => node.parentElement.textContent.replace(whitespaceRegex,'').split('\n'))
 
      tableValueArrays.forEach(tableValueArray => {
-     const className = tableValueArray[1]
-     const creditValue = parseFloat(tableValueArray[tableValueArray.length - 2])
+          const className = tableValueArray[1]
+          const creditValue = parseFloat(tableValueArray[tableValueArray.length - 2])
 
-     switch(className) {
-          case "ENGLISH":
-               result["01 - English"] = creditValue
-               break
-          case "MATH":
-               result["02 - Math"] = creditValue
-               break
-          case "SCIENCE":
-               result["03 - Science"] = creditValue
-               break
-          case "SOCIAL STUDIES".replace(whitespaceRegex,''):
-               result["04 - Social Studies"] = creditValue
-               break
-          case "HEALTH":
-               result["05 - Health"] = creditValue
-               break
-          case "PHYSICAL EDUCATION".replace(whitespaceRegex,''):
-               result["06 - Phy Ed"] = creditValue
-               break
-          case "FINE ARTS".replace(whitespaceRegex,''):
-               result["07 - Fine Arts"] = creditValue
-               break
-          case "OTHER ELECTIVE".replace(whitespaceRegex,''):
-               result["08 - Electives"] = creditValue
-               break
-          case "SERVICE/ONLINE/COMM SERV".replace(whitespaceRegex,''):
-               result["09 - Service Learning"] = creditValue
-               break
-          case "FOREIGN LANGUAGE".replace(whitespaceRegex,''):
-               result["10 - World Language"] = creditValue
-               break   
-          case "COLLEGE AND CAREER READINESS".replace(whitespaceRegex,''):
-               result["11 - Career Readiness"] = creditValue
-               break     
-     }
+          switch(className) {
+               case "ENGLISH":
+                    result["01 - English"] = creditValue
+                    break
+               case "MATH":
+                    result["02 - Math"] = creditValue
+                    break
+               case "SCIENCE":
+                    result["03 - Science"] = creditValue
+                    break
+               case "SOCIAL STUDIES".replace(whitespaceRegex,''):
+                    result["04 - Social Studies"] = creditValue
+                    break
+               case "HEALTH":
+                    result["05 - Health"] = creditValue
+                    break
+               case "PHYSICAL EDUCATION".replace(whitespaceRegex,''):
+                    result["06 - Phy Ed"] = creditValue
+                    break
+               case "FINE ARTS".replace(whitespaceRegex,''):
+                    result["07 - Fine Arts"] = creditValue
+                    break
+               case "OTHER ELECTIVE".replace(whitespaceRegex,''):
+                    result["08 - Electives"] = creditValue
+                    break
+               case "SERVICE/ONLINE/COMM SERV".replace(whitespaceRegex,''):
+                    result["09 - Service Learning"] = creditValue
+                    break
+               case "FOREIGN LANGUAGE".replace(whitespaceRegex,''):
+                    result["10 - World Language"] = creditValue
+                    break   
+               case "COLLEGE AND CAREER READINESS".replace(whitespaceRegex,''):
+                    result["11 - Career Readiness"] = creditValue
+                    break   
+          }
      })
+
+     const transcriptFormValueArrays = Array.from(workspaceDetailDocument.querySelectorAll('.transcriptFormColumn')).map(node => node.parentElement.textContent.replace(whitespaceRegex,'').split('\n'))
+     const totalCreditRow = transcriptFormValueArrays[transcriptFormValueArrays.length - 1]
+     const totalCreditValue = parseFloat(totalCreditRow[totalCreditRow.length - 2])
+
+     result["12 - Total"] = totalCreditValue
 
      console.table(result)
  }
